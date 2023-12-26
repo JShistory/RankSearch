84468e1dba05fcd51603cfa0e5db509c6995ae7c
JShistory <behappy0916@naver.com>
2023-12-26 21:02:17 +0900

    settings

--- src/main/java/com/example/Foods/board/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Foods.board.repository;
-
-import com.example.Foods.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
--- src/main/java/com/example/Foods/member/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Foods.member.repository;
-
-
-import com.example.Foods.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByName(String name);
-}
