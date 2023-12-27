4029ccee9f2fe099face3d5d592b836cfdb6345a
yungughyeon <djaudwls7873@gmail.com>
2023-12-27 20:59:48 +0900

    mock data

--- src/main/java/com/example/Foods/api/ApiController.java
+++ src/main/java/com/example/Foods/api/ApiController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
