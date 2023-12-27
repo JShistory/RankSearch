e6dc4e66e8fba35298f548461f5773042ff4540d
yungughyeon <djaudwls7873@gmail.com>
2023-12-28 01:21:31 +0900

    소환사 정보 헤더

--- front/src/app/summoner/[id]/page.js
+++ front/src/app/summoner/[id]/page.js
@@ -1,7 +1,21 @@
 "use client";
 
+import styled from "styled-components";
+
 const SummonerPage = () => {
-  return <div>:)</div>;
+  return (
+    <div>
+      <div>
+        :)
+        <div>{/* user profile */}</div>
+        <div>{/* tier card */}</div>
+        <div>{/* match */}</div>
+      </div>
+    </div>
+  );
 };
 
 export default SummonerPage;
+
+const S = {};
+S.Wrapper = styled.div``;
