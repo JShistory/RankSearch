517406d7d403cfee05e47ae9d77fa2bdf5cab84d
yungughyeon <djaudwls7873@gmail.com>
2024-01-04 17:38:17 +0900

    kda, 평점, 킬 관여율

--- front/src/components/GameCard.jsx
+++ front/src/components/GameCard.jsx
@@ -9,7 +9,7 @@ import {
   SPELL_IMAGE_ID,
 } from "@/const/api";
 import Image from "next/image";
-import { spellName } from "@/const/image";
+import { spellName } from "@/const/spell";
 
 const GameCard = ({ gameInfo, summonerName }) => {
   const { participants, gameCreation, gameDuration, gameMode } = gameInfo;
@@ -25,6 +25,14 @@ const GameCard = ({ gameInfo, summonerName }) => {
     );
   };
 
+  const winners = participants.filter(
+    (participant) => participant.win === true
+  );
+
+  const losers = participants.filter(
+    (participant) => participant.win === false
+  );
+
   // 자신의 참여 정보를 가져옴
   const mySummoner = findSummoner();
 
@@ -33,7 +41,7 @@ const GameCard = ({ gameInfo, summonerName }) => {
     kills,
     deaths,
     assists,
-    championLevel,
+    champLevel,
     dspell,
     fspell,
     item0,
@@ -43,13 +51,29 @@ const GameCard = ({ gameInfo, summonerName }) => {
     item4,
     item5,
     item6,
+    win,
   } = mySummoner;
+
+  const myTeam = participants.filter((participant) => participant.win === win);
+
+  const totalKills = myTeam.reduce((sum, team) => sum + team.kills, 0);
+  console.log(totalKills);
+
+  const killInvolvement = ((kills + assists) * 100) / totalKills;
+  console.log(Math.round(killInvolvement));
+
+  const kdaGrade = (kills + assists) / deaths;
 
   const myChampion = CHAMPION_IMAGE_URL(championName);
   const myDspell = SPELL_IMAGE_ID(spellName[dspell]);
-  const myFspell = SPELL_IMAGE_ID(fspell);
+  const myFspell = SPELL_IMAGE_ID(spellName[fspell]);
   const myItem0 = ITEM_IMAGE_URL(item0);
   const myItem1 = ITEM_IMAGE_URL(item1);
+  const myItem2 = ITEM_IMAGE_URL(item2);
+  const myItem3 = ITEM_IMAGE_URL(item3);
+  const myItem4 = ITEM_IMAGE_URL(item4);
+  const myItem5 = ITEM_IMAGE_URL(item5);
+  const myItem6 = ITEM_IMAGE_URL(item6);
 
   return (
     <S.Wrapper>
@@ -57,15 +81,25 @@ const GameCard = ({ gameInfo, summonerName }) => {
         <S.Container>
           <S.Content>
             <S.GameMode>
-              <S.QueueType win={mySummoner.win}>{gameMode}</S.QueueType>
+              <S.QueueType win={win}>{gameMode}</S.QueueType>
               <div> {lastPlayDate}</div>
-              <S.Win>{mySummoner.win ? "승리" : "패배"}</S.Win>
+              <S.Win>{win ? "승리" : "패배"}</S.Win>
               <div>{`${minute}분 ${second}초`}</div>
             </S.GameMode>
-            <div>
-              <Image src={myChampion} width={50} height={50} alt="champion" />
+            <S.ChampSpell>
+              <S.Champ>
+                <Image src={myChampion} width={60} height={60} alt="champion" />
+                <S.ChampLevel>{champLevel}</S.ChampLevel>
+              </S.Champ>
               <div>
-                <Image src={myDspell} width={30} height={30} alt="d spell" />
+                <Image src={myDspell} width={36} height={36} alt="d spell" />
+                <Image src={myFspell} width={36} height={36} alt="f spell" />
+              </div>
+            </S.ChampSpell>
+            <div>
+              <div>
+                <div>{`${kills} / ${deaths} / ${assists}`}</div>
+                <div>{`${kdaGrade.toFixed(2)} 평점`}</div>
               </div>
             </div>
           </S.Content>
@@ -102,6 +136,7 @@ S.Content = styled.div`
   height: 100%;
   padding: 10px;
   display: flex;
+  gap: 10px;
 `;
 
 S.GameMode = styled.div`
@@ -123,4 +158,29 @@ S.QueueType = styled.div`
 
 S.Win = styled.div`
   font-weight: 700;
+`;
+
+S.ChampSpell = styled.div`
+  width: 100px;
+  height: 100%;
+  display: flex;
+  gap: 6px;
+`;
+
+S.Champ = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+S.ChampLevel = styled.span`
+  width: 100%;
+  height: 100%;
+  border: 1px solid #000;
+  font-size: 14px;
+  color: #fff;
+  background: #000;
+  text-align: center;
 `;
